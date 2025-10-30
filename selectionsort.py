6f16a97c91ff4b2bb20d055f4c52dc67b6a75c31
Sanjay Murugan M <sanjaymurugan0910@gmail.com>
2025-10-30 15:00:52 +0530

    Selection sort

--- selectionsort.py
+++ selectionsort.py
@@ -1,6 +1,7 @@
 lis=[7,5,4,6,10,12,1,3,9,8,2]
+li1=[]
 for i in range(len(lis)):
-    for j in range(i+1,len(lis)):
-        if lis[i]>lis[j]:
-            lis[i],lis[j]=lis[j],lis[i]
-print(lis)
+    m=min(lis)
+    li1.append(m)
+    lis.remove(m)
+print(li1)
